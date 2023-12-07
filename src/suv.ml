077549bd31d5aa844970c6d4a2ee8b7a3cf2b985
Apollinsa <criquet@insa-toulouse.fr>
2023-12-07 10:44:39 +0100

    c'est plus beau

--- src/suv.ml
+++ src/suv.ml
@@ -17,17 +17,17 @@ let get_max =
 
 
 let  find_path =
- (* g représente le graph, a le point qui est en train d'être évalué,
-    b le poibt d'arriver, l est une liste qui retient la série de point par laquel on est passé*)
-let rec loop acu g a b =
-  let rec arc_loop = function
-    | [] -> raise (No_Pass acu)
-    | {lbl=(flow, cap);_}::q when (cap-flow) == 0 -> arc_loop q                 (*Si l'arc est rempli*)
-    | arc::_ -> try arc::(loop (arc.src::acu) g arc.tgt b)         (*On avance, es si on trouve pas on reviens*)
-                with No_Pass failed_acu -> loop failed_acu g a b                (*On réutilise l'acu qui a fail, comme ça on connait plus vite les points qui vont pas à b*)
+  (* g représente le graph, a le point qui est en train d'être évalué,
+     b le poibt d'arriver, l est une liste qui retient la série de point par laquel on est passé*)
+  let rec loop acu g a b =
+    let rec arc_loop = function
+      | [] -> raise (No_Pass acu)
+      | {lbl=(flow, cap);_}::q when (cap-flow) == 0 -> arc_loop q    (*Si l'arc est rempli*)
+      | arc::_ -> try arc::(loop (arc.src::acu) g arc.tgt b)         (*On avance, es si on trouve pas on reviens*)
+        with No_Pass failed_acu -> loop failed_acu g a b             (*On réutilise l'acu qui a fail, comme ça on connait plus vite les points qui vont pas à b*)
+    in
+    if a = b then []                                                (*Si on est arrivé on renvoie juste l*)
+    else if List.mem a acu then raise (No_Pass acu)                 (*Si on est déja passé par ce point on remonte*)
+    else arc_loop (out_arcs g a)                                    (*On évalue la liste des arcs sortant de a*)
   in
-  if a = b then []                                                  (*Si on est arrivé on renvoie juste l*)
-           else if List.mem a acu then raise (No_Pass acu)                 (*Si on est déja passé par ce point on remonte*)
-                             else arc_loop (out_arcs g a)           (*On évalue la liste des arcs sortant de a*)
-in
-    loop []
+  loop []
--- src/tools.ml
+++ src/tools.ml
@@ -5,7 +5,7 @@ let clone_nodes gr = Graph.n_fold gr Graph.new_node Graph.empty_graph
 let gmap gr f = Graph.e_fold gr (fun g a -> Graph.new_arc g {src=a.src;tgt=a.tgt;lbl=f a.lbl}) (clone_nodes gr)
 
 let add_arc g id1 id2 n =
-match Graph.find_arc g id1 id2 with
-| None -> Graph.new_arc g {src =id1; tgt=id2; lbl=n}
-| Some x -> Graph.new_arc g {src =id1; tgt=id2; lbl=(n+x.lbl)};
+  match Graph.find_arc g id1 id2 with
+  | None -> Graph.new_arc g {src =id1; tgt=id2; lbl=n}
+  | Some x -> Graph.new_arc g {src =id1; tgt=id2; lbl=(n+x.lbl)};
 
