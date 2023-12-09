091291da509be3dafb12f993ec7fffebd5e5d0fa
Jean-Yves S <saint-lo@insa-toulouse.fr>
2023-12-09 12:06:07 +0100

    improve find path

--- src/suv.ml
+++ src/suv.ml
@@ -20,23 +20,22 @@ let min_flow i ar = min i ar.lbl.spc
 
 
 (* g représente le graph, a le point qui est en train d'être évalué,
- b le poibt d'arriver, acu est une liste qui retient la série de point par laquel on est passé*)
-let  find_path g (first, b)=
-  let rec next_node acu a =
-    let rec arc_loop acu2 = function
-      (*Si il n'y a plus d'arc à exploré on lève une exeption avec la liste des neuds explorés*)
-      | [] -> raise (No_Path acu2)
-      (*Si l'arc est rempli on l'ignore ou
-       *Si on est déjà passé par l'arc on l'ignore*)
-      | h::q when (h.lbl.spc == 0) || (List.mem h.tgt acu2) -> arc_loop acu2 q
-      (*On avance, es si on trouve pas on reviens*)
-      | h::q -> try h::(next_node (h.src::acu2) h.tgt)
-        (*On réutilise l'acu qui a fail, comme ça on connait plus vite les points qui vont pas à b*)
-        with No_Path failed_acu -> arc_loop failed_acu q
-
-    (*Si on est arrivé on renvoie juste l sinon on évalue la liste des arcs sortant de a*)
-    in if a = b then [] else arc_loop acu (out_arcs g a)
-  in next_node [] first
+   b le point d'arriver, acu est une liste qui retient la série de point par laquel on est passé*)
+let  find_path g (a, b) =
+  let rec arc_loop acu = function
+    (* Si il n'y a plus d'arc à exploré on lève une exception avec la liste des neuds explorés *)
+    | [] -> raise (No_Path acu)
+    (* Si l'arc est rempli on l'ignore *)
+    | h::q when h.lbl.spc == 0 -> arc_loop acu q
+    (* Si le prochain neud et l'arrivé on termine l'algorithm *)
+    | h::_ when h.tgt == b -> [h]
+    (* Si on est déjà passé par l'arc on l'ignore *)
+    | h::q when List.mem h.tgt acu -> arc_loop acu q
+    (* On avance, et si on trouve pas on reviens*)
+    | h::q -> try h::(arc_loop (h.tgt::acu) (out_arcs g h.tgt))
+      (*On réutilise l'acu qui a fail, comme ça on connait plus vite les points qui vont pas à b*)
+      with No_Path failed_acu -> arc_loop failed_acu q
+  in arc_loop [a] (out_arcs g a)
 
 let add_flow g {src=src;tgt=tgt;lbl={cap=cap;use=use;spc=spc}} i =new_arc (new_arc g {src=src;tgt=tgt;lbl={cap=cap;use=use+i;spc=spc-i}}) {src=tgt;tgt=src;lbl={cap=cap;use=spc-i;spc=use+i}}
 
