c99b9f0b99afafcf5146553809286c23e33a3a88
Jean-Yves S <saint-lo@insa-toulouse.fr>
2023-12-18 21:22:36 +0100

    update parser

--- src/suvFile.ml
+++ src/suvFile.ml
@@ -28,7 +28,7 @@ let read_file path =
       else try match line.[0] with
         | 'D' -> Scanf.sscanf line "D %s %s" (new_dep_node acu)
         | 'A' -> Scanf.sscanf line "A %s %s" (new_arr_node acu)
-        | 'I' -> Scanf.sscanf line "I %s %s %s" (new_choice_arc acu)
+        | 'L' -> Scanf.sscanf line "L %s %s %s" (new_choice_arc acu)
         | '%' -> acu
         | _ -> failwith "unknow char"
         with e -> Printf.printf "Cannot read line - %s:\n%s\n%!" (Printexc.to_string e) line ; failwith "from_file"
@@ -42,19 +42,15 @@ let read_file path =
 let export path dict g =
   (* Open a write-file. *)
   let ff = open_out path in
-  let loop arc = Printf.fprintf ff "    %s -> %s [xlabel = \"%s\", style = \"%s\"];\n"
+  let loop arc = Printf.fprintf ff "    %s->%s[xlabel=\"%s\",style=%s];\n"
   (find_name arc.src dict) (find_name arc.tgt dict) (string_of_int arc.lbl.use) (if arc.lbl.use = 0 then "dotted" else if arc.lbl.spc = 0 then "bold" else "solid" )
   in
 
   (* Write in this file. *)
-  Printf.fprintf ff "digraph finite_state_machine {\n";
-  Printf.fprintf ff "    graph [pad=\"0.5\", nodesep=\"1\", ranksep=\"2\"];\n";
-  Printf.fprintf ff "    splines=false;\n";
-  Printf.fprintf ff "    fontname=\"Helvetica,Arial,sans-serif\";\n";
-  Printf.fprintf ff "    node[fontname=\"Helvetica,Arial,sans-serif\"];\n";
+  Printf.fprintf ff "digraph {\n";
+  Printf.fprintf ff "    graph[pad=0.5,nodesep=1,ranksep=2,rankdir=LR,splines=false];\n";
+  Printf.fprintf ff "    node[fontname=\"Helvetica,Arial,sans-serif\",shape=box];\n";
   Printf.fprintf ff "    edge[fontname=\"Helvetica,Arial,sans-serif\"];\n";
-  Printf.fprintf ff "    rankdir=LR;\n";
-  Printf.fprintf ff "    node [shape = box];\n";
 
   (* Write all arcs *)
   Tools.arc_iter loop g;
